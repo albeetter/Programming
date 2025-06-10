ad781c472379a8d5e397abb52f979d363a502f9a
Julius Albert Ortiz <150880431+albeetter@users.noreply.github.com>
2025-06-10 10:03:05 +0800

    Add files via upload

---
+++ employee/Employeeinfo.java
@@ -0,0 +1,12 @@
+package employee;
+
+public class Employeeinfo {
+	public void display_Employee(String employee, String designation, String position) {
+		System.out.println("EMPLOYEE DETAILS");
+		System.out.println("Employee Name: " + employee);
+		System.out.println("Designation: " + designation);
+		System.out.println("Academic Position: " + position);
+		System.out.println();
+	}
+
+}
